eb58380ae30177065bb28ad799f5498ba0ebc94a
Craig Regnier <regs79@gmail.com>
2024-06-14 14:19:00 -0400

    Fix robots.txt and add sitemap generator.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,13 @@
+'use strict'
+
+const { format: dateFnsFormat } = require('date-fns');
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addNunjucksFilter("date", function(date, dateFormat) {
+    return dateFnsFormat(date, dateFormat)
+  });
+
   eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
-	});
+        "./public/": "/",
+    });
 }
