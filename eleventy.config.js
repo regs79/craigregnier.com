ed6efb8f6423480c06ef4fbe2954168f75aef1a3
Craig Regnier <regs79@gmail.com>
2024-06-14 16:59:39 -0400

    Inline css and update layout to a nunjucks template

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const { format: dateFnsFormat } = require('date-fns');
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addNunjucksFilter("date", function(date, dateFormat) {
     return dateFnsFormat(date, dateFormat)
   });
+
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
   eleventyConfig.addPassthroughCopy({
         "./public/": "/",
